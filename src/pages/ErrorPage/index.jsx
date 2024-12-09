d1e4904c0643e44e45fdd5b845b43caff919debe
Ibrohimjon-Kurbanov <kurbanovolddev@gmail.com>
2024-12-09 13:57:47 +0200

    comfy-store

--- src/pages/ErrorPage/index.jsx
+++ src/pages/ErrorPage/index.jsx
@@ -1,5 +1,9 @@
-import "./index.css";
 function ErrorPage() {
-  return <div>ErrorPage</div>;
+  return (
+    <div className="container flex items-center justify-center  min-h-[calc(100vh-98px)]">
+      <h1 className="text-[40px] text-red-500 font-semibold">Error page</h1>
+    </div>
+  );
 }
+
 export default ErrorPage;
